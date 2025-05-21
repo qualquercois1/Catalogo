ea9872a1cf45a95f1aa8977c95528f6ba386941f
qualquercois1 <95540574+qualquercois1@users.noreply.github.com>
2025-05-21 17:56:36 -0300

    Delete projeto1/catalogo directory

--- projeto1/catalogo/app_catalogo/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    return render(request, 'usuarios/home.html')
-
-def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
--- projeto1/catalogo/catalogo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app_catalogo import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('cadastro/', views.cadastro, name='cadastro')
-]
